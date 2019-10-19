63af4daad8a5cacd8da8405f935e6f59f59883eb
BamButz <bambutz@cryptonica.de>
2019-10-19 13:22:43 +0200

    Complete observer pattern

--- src/design-patterns/observer.ts
+++ src/design-patterns/observer.ts
@@ -1,18 +1,50 @@
-//incomplete
+interface Subject {
+  attach(observer: Observer): void;
+  detach(observer: Observer): void;
+  notify(): void
+}
 
-class Subject {
-  private observerList: Observer[];
-  private _state: number;
+class Citizen implements Subject {
+  private _isCriminal: boolean = false;
+  private _observers: Observer[] = [];
 
-  public get state(): number {
-    return this._state; 
+  public attach(observer: any): void {
+    this._observers.push(observer);
+  }
+  
+  public detach(observer: any): void {
+    const observerIndex = this._observers.indexOf(observer);
+    this._observers.splice(observerIndex, 1);
+  }
+
+  public notify(): void {
+    for(const observer of this._observers) {
+      observer.update(this);
+    }
+  }
+
+  public breakIntoStore(): void {
+    this._isCriminal = true;
+    this.notify();
+  }
+
+  public isCriminal(): boolean {
+    return this._isCriminal;
   }
 }
 
-interface Observable {
-  update();
+interface Observer {
+  update(subject: Subject): void;
 }
 
-class Observer implements Observable {
+class Police implements Observer {
+  private _wanted: Citizen[] = []; 
 
+  public update(subject: Subject): void {
+    if(subject instanceof Citizen) {
+      if(subject.isCriminal()) {
+        this._wanted.push(subject);
+      }
+    }
+  }
 }
